ddbd989579a0067a0d7fd2a12ae63e77c36bd6c8
William Brendel <brendel.william@gmail.com>
2024-06-27 16:39:32 -0700

    utils

--- src/utils/style.mjs
+++ src/utils/style.mjs
@@ -1,6 +1,6 @@
 import head from './head.mjs';
 
 // Create head node if needed.
-export const style = (head.getElementsByTagName() || [])[0]
+export const style = (head.getElementsByTagName('style') || [])[0]
   || (head.appendChild(document.createElement('style')));
 export default style;
