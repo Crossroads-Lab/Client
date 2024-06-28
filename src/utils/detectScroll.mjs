fcba7cac2e40431f619257185a382966bf8abfd4
William Brendel <brendel.william@gmail.com>
2024-06-28 12:41:29 -0700

    detectScroll

--- src/utils/detectScroll.mjs
+++ src/utils/detectScroll.mjs
@@ -1,11 +1,16 @@
 // Imports.
 import { throttle } from 'https://crossroads-lab.github.io/Utils/src/index.mjs';
 
-// Helper function to detect scroll on an.
-export const detectScroll = (elmt = document.body) => {
-  elmt && elmt.addEventListener('scroll', throttle(() => {
-    elmt && (elmt.dataset.scrolling = elmt.scrollTop > 0 || elmt.scrollLeft > 0);
-  }), { passive: true });
+// Helper function to detect scroll on an element.
+const cb = throttle(event => (
+  event.target.dataset.scrolling = event.target.scrollTop > 0 || event.target.scrollLeft > 0
+));
+
+export const detectScroll = (elmt = document.body, options = { passive: true }) => {
+  elmt && (
+    elmt.removeEventListener('scroll', cb, options),
+    elmt.addEventListener('scroll', cb, options)
+  )
 }
 
 // Default export.
