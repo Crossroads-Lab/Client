117caf1420ea7e1cc41ffb7562dd3c8db55dff69
William Brendel <brendel.william@gmail.com>
2024-06-28 10:30:56 -0700

    getscriptnode

---
+++ src/utils/getScriptNode.mjs
@@ -0,0 +1,20 @@
+// Helper function to get a script node.
+export const getScriptNode = src => {
+  if (!src) {
+    try {
+      src = import.meta.url;
+    } catch {
+      return document.currentScript;
+    }
+  }
+
+  const scripts = document.getElementsByTagName('script');
+  for (let i = 0, l = scripts.length, s; i !== l; ++i) {
+    if (src === (s = scripts[i]).src || s.getAttribute('src')) return s;
+  }
+
+  return null;
+}
+
+// Default export.
+export default getScriptNode;
