6852b1c8c47ab74faf65d25fa63abc558a7ccac5
William Brendel <brendel.william@gmail.com>
2024-06-28 11:14:22 -0700

    comment

--- src/utils/addComment.mjs
+++ src/utils/addComment.mjs
@@ -3,7 +3,7 @@ import head from './head.mjs';
 
 // Helper function to add a meta tags in the document head.
 export const addComment = (comment, node = head) => (
-  comment && node instanceof Node && node.appendChild(document.createComment(`${comment}`)) || null
+  comment && node instanceof Node && node.appendChild(document.createComment(` ${comment.trim()} `)) || null
 )
 
 // Default export.
