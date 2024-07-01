ef349c7819bc7f4737ef0a54fce367d7169cc7eb
William Brendel <brendel.william@gmail.com>
2024-07-01 16:58:41 -0700

    contact us

--- src/utils/detectScroll.mjs
+++ src/utils/detectScroll.mjs
@@ -3,7 +3,7 @@ import { throttle } from 'https://crossroads-lab.github.io/Utils/src/index.js';
 
 // Helper function to detect scroll on an element.
 const cb = throttle(event => (
-  event.target.dataset.scrolling = event.target.scrollTop > 0 || event.target.scrollLeft > 0
+  (event.target === window && document.body || event.target).dataset.scrolling = event.target.scrollTop > 0 || event.target.scrollLeft > 0
 ));
 
 export const detectScroll = (elmt = window, handler = cb, options = { passive: true }) => {
