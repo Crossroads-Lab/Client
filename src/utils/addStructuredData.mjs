368e9a9e032bf79bf0cb500c5b197e6c18b7d265
William Brendel <brendel.william@gmail.com>
2024-07-05 14:03:57 -0700

    structured data

--- src/utils/addStructuredData.mjs
+++ src/utils/addStructuredData.mjs
@@ -4,11 +4,12 @@ import addElement from './addElement.mjs';
 
 // Helper function to add a meta tags in the document head.
 export const addStructuredData = props => {
-  let {tag, tagName, parent, parentNode, type, data, ...other} = props || {};
+  let {tag, tagName, parent, parentNode, type, data, comment, ...other} = props || {};
   return (data || (data = other)) && addElement({
     tag: 'script',
     parent: head,
     type: 'application/ld+json',
+    comment,
     innerHTML: JSON.stringify(data),
   }) || null;
 }
